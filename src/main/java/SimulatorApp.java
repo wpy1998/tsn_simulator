212c368115f9407e68bb59dc36f1d9eeb9b112ab
wpy1998 <1251499395@qq.com>
2022-05-17 02:06:37 +0800

    commit by wpy

--- src/main/java/SimulatorApp.java
+++ src/main/java/SimulatorApp.java
@@ -24,6 +24,7 @@ public class SimulatorApp {
         Computer computer = new Computer();
         SquareLauncher launcher = new SquareLauncher();
         launcher.init(computer);
-        launcher.start(20);
+        launcher.start(75);
     }
+    //5, 10, 15, 20, 25
 }
